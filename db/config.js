01d7b22bdef5e358593f84959e0c18f8511e858a
sakshigupta001 <sakshiguptadav@gmail.com>
2022-12-03 10:49:27 +0530

    ME_BUILDOUT_XHARKTANK_MODULE_BASIC

--- db/config.js
+++ db/config.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 // const pitchModel= require("../db/pitchSchema");
 // const offerModel= require("../db/offerSchema");
 //const MONGO_CONNECTION_URL= "mongodb://localhost:27017/xharktank";
+const pitches= require("../db/models");
 
 async function deleteCollection(){
     const db = mongoose.connection.db;
@@ -15,22 +16,26 @@ async function deleteCollection(){
       .map((collection) => collection.name)
       .forEach(async (collectionName) => {
         if(collectionName=='pitches' || collectionName=='offers')
-            db.dropCollection(collectionName);
+        {
+            //db.dropCollection(collectionName);
+            //db.collectionName.deleteMany({});
+        }      
       });
 }
 
 function connectDB(){
 
-    mongoose.connect(process.env.MONGO_CONNECTION_URL, { serverSelectionTimeoutMS: 10000, connectTimeoutMS:10000, socketTimeoutMS:10000, useNewUrlParser: 
+    mongoose.connect("mongodb://localhost:27017/xharktank", { serverSelectionTimeoutMS: 10000, connectTimeoutMS:10000, socketTimeoutMS:10000, useNewUrlParser: 
         true, useUnifiedTopology: true});
 
         const connection= mongoose.connection;
 
-        connection.once('open', function(){
+        connection.once('open', async function(){
             console.log("DATABASE CONNECTED");
             // pitchModel.deleteMany({});
             // offerModel.deleteMany({});
-            deleteCollection();
+            await pitches.deleteMany({});
+            //deleteCollection();
         }).on('error', function(err){
             console.log("DATABASE CONNECTION FAILED");
         })
